3dd5cd32e0c59c56a6152a2b1022e0437bf099a3
Guillaume Jacquet <guillaume.jacquet@appdirect.com>
2014-05-26 16:10:03 -0400

    loading environment from WORKSPACE directory

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-. environment.sh
+. $WORKSPACE/environment.sh
 . $HOME/.rvm/scripts/rvm
 
-echo `ruby --version`
-echo rvm use $RUBY_VERSION
 rvm use $RUBY_VERSION
-echo `ruby --version`
+echo "Using $(ruby --version)"
 
-type rvm | head -n 1
 rvm user gemsets
 gem install bundler
 bundle install
 
 cd $WORKSPACE 
 rspec services_spec/cleardb_isv_spec.rb
+
